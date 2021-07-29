4062d047be86fe601178d3a6b73a764e2661d706
smitparmar33 <smit.p@itindia.co.in>
2021-07-29 11:24:33 +0530

    urls.py changed

--- blog/urls.py
+++ blog/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.static import serve 
+from django.conf.urls import url
 
 from article import views
 urlpatterns = [
@@ -26,5 +27,8 @@ urlpatterns = [
     path('about/',views.about,name = "about"),
     path('articles/',include("article.urls")),
     path('user/',include("user.urls")),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
